cc4f9fe5f7228340803e6634ab17c728f2e3efa1
Filipe Correia <filipe.lp.correia@gmail.com>
2019-01-20 23:26:23 +0000

    Filter cards over result set

--- move.rb
+++ move.rb
@@ -15,12 +15,13 @@ end
 lists = get "boards/#{board_id}/lists?cards=none&card_fields=all&filter=open&fields=all"
 
 dest_id =  lists.find{|e| e["name"] == dest}["id"]
+origin_id = lists.find{|e| e["name"] == origin}["id"]
 
 labelQuery = labels.map{|e| "label:#{e}"}.join("&")
+query = labelQuery + ["list:#{origin}", "modelTypes=cards"].join("&")
 
-search = get "search?card_fields=name,id&cards_limit=1000&query=#{labelQuery}&list:#{origin}&modelTypes=cards"
+search = get "search?card_fields=name,id,labels,idList&cards_limit=1000&query=#{query}"
 
-
-search["cards"].each do |c|
+search["cards"].select{|e| e["idList"] == origin_id && ((labels - e["labels"].map{|e| e["name"]}).length  == 0)}.each do |c|
     put "cards/#{c["id"]}?idList=#{dest_id}"
 end
